2fa7cd610ac445d4c88a62703c52ccc40b9862de
sinanenes <sinanenes86@gmail.com>
2021-12-06 02:31:14 +0300

    kasabanka_ClassAdded

---
+++ DernekYonetimiAPI/Models/KasaBanka.cs
@@ -0,0 +1,11 @@
+﻿namespace DernekYonetimiAPI.Models
+{
+    public class KasaBanka
+    {
+        public int KasaBankaId { get; set; }
+        public string? KasaBankaAdi { get; set; }
+        public int? KasaBankaTurKod { get; set; }
+        public string? Aciklama { get; set; }
+
+    }
+}
